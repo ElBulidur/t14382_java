bfcc697da09fa717923a33d8c08b50ca0e646940
Julio Pereira <juliopereira.dev@gmail.com>
2023-08-23 19:41:19 -0300

    inicio da aula 06

---
+++ questoes/repostas.java
@@ -0,0 +1,15 @@
+package questoes;
+
+public class repostas {
+    public static void main(String[] args) {
+        int colorOfRainbow = 10;
+        red = 5;
+        switch(colorOfRainbow) {
+            default:
+            System.out.print("Home");
+            break;
+            case red:
+            System.out.print("Away");
+        }
+    }
+}
